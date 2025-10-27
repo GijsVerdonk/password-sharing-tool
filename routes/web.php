168b77b397dedd2569086ee51babc15884b1ed8d
Gijs Verdonk <g.verdonk@icloud.com>
2025-10-27 18:53:19 +0100

    Add Secret Model, Controller and Migration.

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\SecretController;
 
 Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
+
+Route::get('/secrets', [SecretController::class, 'index']);
